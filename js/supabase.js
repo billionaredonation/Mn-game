ee0fda1a9040e58ac25e8a8f76e3fe3a9d1c66dc
billionaredonation <5fermatop@gmail.com>
2026-04-19 00:03:24 +0200

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://nvwpdslwltayriyibwis.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_ea3dZcU5jl6wK2-qiB3NaQ_m4td004I";
+
+window.sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
