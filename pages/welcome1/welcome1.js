67d78a19bcd347ce71304489f5ee9f0ea6075921
billionaredonation <5fermatop@gmail.com>
2026-04-24 12:30:42 +0200

    Update welcome1.js

--- pages/welcome1/welcome1.js
+++ pages/welcome1/welcome1.js
@@ -1,1 +1,11 @@
-javascript import { register, show } from '../../src/router.js'; register('welcome1', (root) => { root.className = 'page welcome1'; root.innerHTML = `<h1>Привет, новичок!</h1> <p>Текст можно поменять позже.</p> <button class="btn">Далее</button>`; root.querySelector('.btn').onclick = () => show('welcome2'); });
+import { register, show } from '../../src/router.js';
+
+register('welcome1', (root) => {
+  root.className = 'page welcome1';
+  root.innerHTML = `
+    <h1>Привет, новичок!</h1>
+    <p>Здесь позже появится красивый текст-затравка.</p>
+    <button class="btn">Далее</button>
+  `;
+  root.querySelector('.btn').onclick = () => show('welcome2');
+});
