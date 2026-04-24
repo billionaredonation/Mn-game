e66fc0cb4b9ae4f89a8d1a913da3f87208410f54
billionaredonation <5fermatop@gmail.com>
2026-04-24 12:25:36 +0200

    Create welcome1.js

---
+++ pages/welcome1/welcome1.js
@@ -0,0 +1,1 @@
+javascript import { register, show } from '../../src/router.js'; register('welcome1', (root) => { root.className = 'page welcome1'; root.innerHTML = `<h1>Привет, новичок!</h1> <p>Текст можно поменять позже.</p> <button class="btn">Далее</button>`; root.querySelector('.btn').onclick = () => show('welcome2'); });
