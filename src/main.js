7199d0cf41aee7a4cc0a5d99329b906786f1457b
billionaredonation <5fermatop@gmail.com>
2026-04-24 13:10:12 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
-import { show } from './router.js?v=3';
-import { state } from './state.js?v=3';
+import { show } from './router.js?v=4';
 
-import '../pages/welcome1/welcome1.js?v=3';
-import '../pages/welcome2/welcome2.js?v=3';
-import '../pages/welcome3/welcome3.js?v=3';
-import '../pages/home/home.js?v=3';
+import '../pages/welcome1/welcome1.js?v=4';
+import '../pages/welcome2/welcome2.js?v=4';
+import '../pages/welcome3/welcome3.js?v=4';
+import '../pages/home/home.js?v=4';
 
 window.Telegram?.WebApp?.expand();
 
-if (!state.nickname) {
-  show('welcome1');
-} else if (!state.city) {
-  show('welcome3');
-} else {
-  show('home');
-}
+/*
+  ВРЕМЕННО ДЛЯ ТЕСТА:
+  Каждый запуск очищает прогресс и кидает на welcome1.
+  Потом уберём, когда нормально настроим выбор города.
+*/
+localStorage.removeItem('player');
+
+show('welcome1');
