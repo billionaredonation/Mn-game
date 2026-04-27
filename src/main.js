2237dee4923044cac6aa5c992ed7c77bf2422b3f
billionaredonation <5fermatop@gmail.com>
2026-04-27 12:26:43 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { state } from './state.js?v=43';
 
 import '../pages/welcome1/welcome1.js?v=43';
 import '../pages/welcome2/welcome2.js?v=43';
-import '../pages/welcome3/welcome3.js?v=43';
+import '../pages/welcome3/welcome3.js?v=44';
 import '../pages/home/home.js?v=43';
 
 window.Telegram?.WebApp?.expand();
