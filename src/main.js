8e094660e2c8cfc02410acef1814fba4349ab2d4
billionaredonation <5fermatop@gmail.com>
2026-04-28 14:41:35 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { show } from './router.js';
 import { state } from './state.js';
 
-import '../pages/welcome1/welcome1.js?v=60';
+import '../pages/welcome1/welcome1.js?v=61';
 import '../pages/welcome2/welcome2.js?v=60';
 import '../pages/welcome3/welcome3.js?v=60';
 import '../pages/home/home.js?v=20';
