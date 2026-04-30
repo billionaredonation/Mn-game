634388f4aa14fe37b4ee0418172a7fd42ffd053b
billionaredonation <5fermatop@gmail.com>
2026-04-30 18:37:52 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { show } from './router.js';
 import { initRuntime, getState } from './state.js';
 
-import '../pages/welcome1/welcome1.js?v=75';
-import '../pages/welcome2/welcome2.js?v=75';
-import '../pages/welcome3/welcome3.js?v=75';
-import '../pages/home/home.js?v=75';
+import '../pages/welcome1/welcome1.js';
+import '../pages/welcome2/welcome2.js';
+import '../pages/welcome3/welcome3.js';
+import '../pages/home/home.js';
 
-window.Telegram?.WebApp?.expand();
+window.Telegram?.WebApp?.expand?.();
 
 initRuntime();
 
