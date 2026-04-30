b7e0d42357967614f3645cc128a9de0fe1bb387a
billionaredonation <5fermatop@gmail.com>
2026-04-30 23:51:02 +0200

    Update welcome2.js

--- pages/welcome2/welcome2.js
+++ pages/welcome2/welcome2.js
@@ -1,5 +1,5 @@
-import { register, show } from '../../src/router.js';
-import { state, save } from '../../src/state.js';
+import { register, show } from '../../src/router.js?v=37';
+import { state, save } from '../../src/state.js?v=37';
 
 register('welcome2', (root) => {
   root.className = 'page welcome2';
