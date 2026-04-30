11ee3cd5e004a8ab621706c92c84de949a31a2fb
billionaredonation <5fermatop@gmail.com>
2026-05-01 00:05:55 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import { show } from './router.js?v=37';
-import { initRuntime, getState } from './state.js?v=37';
+import { show } from './router.js?v=80';
+import { initRuntime, getState } from './state.js?v=80';
 
-import '../pages/welcome1/welcome1.js?v=37';
-import '../pages/welcome2/welcome2.js?v=37';
-import '../pages/welcome3/welcome3.js?v=37';
-import '../pages/home/home.js?v=37';
+import '../pages/welcome1/welcome1.js?v=80';
+import '../pages/welcome2/welcome2.js?v=80';
+import '../pages/welcome3/welcome3.js?v=80';
+import '../pages/home/home.js?v=80';
 
 if (window.Telegram && window.Telegram.WebApp && window.Telegram.WebApp.expand) {
   window.Telegram.WebApp.expand();
@@ -13,10 +13,12 @@ if (window.Telegram && window.Telegram.WebApp && window.Telegram.WebApp.expand) 
 initRuntime();
 
 const st = getState();
+const nickname = st.nickname || (st.player && st.player.nickname);
+const city = st.city || (st.player && st.player.city);
 
-if (!st.nickname) {
+if (!nickname) {
   show('welcome1');
-} else if (!st.city) {
+} else if (!city) {
   show('welcome3');
 } else {
   show('home');
